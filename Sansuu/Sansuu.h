bca143b4147be38d9dfbad0cccbc6c15798e3bc2
maiyama18 <myam0118@gmail.com>
2021-05-29 13:18:53 +0900

    Initial Commit

---
+++ Sansuu/Sansuu.h
@@ -0,0 +1,18 @@
+//
+//  Sansuu.h
+//  Sansuu
+//
+//  Created by yam118 on 2021/05/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sansuu.
+FOUNDATION_EXPORT double SansuuVersionNumber;
+
+//! Project version string for Sansuu.
+FOUNDATION_EXPORT const unsigned char SansuuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sansuu/PublicHeader.h>
+
+
