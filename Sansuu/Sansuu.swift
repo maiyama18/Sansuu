8a5feb3aa0377b4ec39e6758ff5945d943eb9594
maiyama18 <myam0118@gmail.com>
2021-05-30 13:40:19 +0900

    init

---
+++ Sansuu/Sansuu.swift
@@ -0,0 +1,14 @@
+//
+//  Sansuu.swift
+//  Sansuu
+//
+//  Created by yam118 on 2021/05/29.
+//
+
+public func add(_ n1: Int, _ n2: Int) -> Int {
+    n1 + n2
+}
+
+public func subtract(_ n1: Int, _ n2: Int) -> Int {
+    n1 - n2
+}
